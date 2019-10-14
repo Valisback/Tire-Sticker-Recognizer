dc83f068dfc41f560773ebfe7c3ff0c1b48471de
Valentin <49312215+Vale-polito@users.noreply.github.com>
2019-10-14 14:04:51 -0400

    Implementing Firebase Database

---
+++ app/src/main/java/com/example/tirestickerrecognition/ListScan.java
@@ -0,0 +1,14 @@
+package com.example.tirestickerrecognition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListScan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_scan);
+    }
+}
